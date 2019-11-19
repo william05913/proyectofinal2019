de00024c306319bb2c3b2ab8a80338892b2a0417
Luis1422-del <alberto14rivera19@gmail.com>
2019-11-18 21:20:38 -0600

    agregue la clase AlabanzasActivity

---
+++ app/src/main/java/com/example/proyectofinal/AlabanzasActivity.java
@@ -0,0 +1,15 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlabanzasActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alabanzas);
+    }
+}
