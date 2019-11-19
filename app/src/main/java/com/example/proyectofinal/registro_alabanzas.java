9773e66fe165c53edf48c657b6a219c403ca414a
valladaresEspinoza <valladaresjoseeduardo722@gmail.com>
2019-11-18 23:31:34 -0600

    Declarando variables para Registro de alabanzas

--- app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
+++ app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
@@ -1,10 +1,21 @@
 package com.example.proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class registro_alabanzas extends AppCompatActivity {
+    private ListView lvdatos;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
+
+    EditText buscar;
+
+    AlertDialog.Builder dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
