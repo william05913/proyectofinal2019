56df4316b7effa3b05a3c9eb2e7c8c9bacd4e7a1
valladaresEspinoza <valladaresjoseeduardo722@gmail.com>
2019-11-18 22:56:01 -0600

    Adicione los metodos get y set

--- app/src/main/java/com/example/proyectofinal/Coros.java
+++ app/src/main/java/com/example/proyectofinal/Coros.java
@@ -8,9 +8,43 @@ public class Coros {
 
     public Coros() {
     }
+
     public Coros(int id, String titulo, String autor, String letra) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
         this.letra = letra;
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra; }
+
 }
