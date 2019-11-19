53b50da2d23ba0381269e5cd0bd1bdbf65c590b2
Alexander12211997 <bonillaanderson304.07@gmail.com>
2019-11-19 08:11:06 -0600

    cree la clase registro_coros

---
+++ app/src/main/java/com/example/proyectofinal/registro_coros.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_coros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_coros);
+    }
+}
