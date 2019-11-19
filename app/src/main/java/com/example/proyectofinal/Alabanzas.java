a9527a29aa38417beca9a0da8f0e5b4ce3beba6c
william50913 <william29amaya@gmail.com>
2019-11-18 20:58:46 -0600

    agregue los metodos get y set

--- app/src/main/java/com/example/proyectofinal/Alabanzas.java
+++ app/src/main/java/com/example/proyectofinal/Alabanzas.java
@@ -14,4 +14,35 @@ public class Alabanzas {
         this.autor = autor;
         this.letra = letra;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
 }
