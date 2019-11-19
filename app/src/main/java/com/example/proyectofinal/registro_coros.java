c87065ba2e06d5fe5a15d1a3d728c1cf9309f780
Alexander12211997 <bonillaanderson304.07@gmail.com>
2019-11-19 08:13:01 -0600

    le agregue variable

--- app/src/main/java/com/example/proyectofinal/registro_coros.java
+++ app/src/main/java/com/example/proyectofinal/registro_coros.java
@@ -1,10 +1,20 @@
 package com.example.proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class registro_coros extends AppCompatActivity {
+    private AsyncHttpClient clientec = new AsyncHttpClient();
+    private ListView lvdatosc;
+
+    AlertDialog.Builder dialogo;
+    EditText buscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
