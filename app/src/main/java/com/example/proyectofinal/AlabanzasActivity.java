2094fbc88ffbcfb10148ba7170d7387459f3f3d5
Luis1422-del <alberto14rivera19@gmail.com>
2019-11-18 21:23:45 -0600

    agregue variables

--- app/src/main/java/com/example/proyectofinal/AlabanzasActivity.java
+++ app/src/main/java/com/example/proyectofinal/AlabanzasActivity.java
@@ -1,11 +1,21 @@
 package com.example.proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class AlabanzasActivity extends AppCompatActivity {
 
+    private EditText ettitulo, etautor, etletra;
+    private Button btnRegistrar;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
+    private ListView lvdatos;
+
+    AlertDialog.Builder dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
