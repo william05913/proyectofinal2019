5b3e590f9103778b9191527c5cf8dc5c3ab78b7c
valladaresEspinoza <valladaresjoseeduardo722@gmail.com>
2019-11-18 23:07:42 -0600

    Declarando Variables

--- app/src/main/java/com/example/proyectofinal/CorosActivity.java
+++ app/src/main/java/com/example/proyectofinal/CorosActivity.java
@@ -1,10 +1,23 @@
 package com.example.proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class CorosActivity extends AppCompatActivity {
+
+    private EditText ettituloc, etautorc, etletrac;
+    private Button btnRegistrarc;
+    private AsyncHttpClient clientec = new AsyncHttpClient();
+    private ListView lvdatosc;
+
+    AlertDialog.Builder dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
