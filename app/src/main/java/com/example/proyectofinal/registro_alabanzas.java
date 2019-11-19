da24a6a94dae355a76321561ec62960f0d4f2136
Alexander12211997 <bonillaanderson304.07@gmail.com>
2019-11-19 07:39:36 -0600

    agregue el metodo obtenerAlabanzas

--- app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
+++ app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class registro_alabanzas extends AppCompatActivity {
     private ListView lvdatos;
@@ -56,4 +59,20 @@ public class registro_alabanzas extends AppCompatActivity {
 
     }
 
+    private void obtenerAlabanzas(){
+        String url = "https://proyectofinalsis21.000webhostapp.com/obtenerDatos.php";
+        cliente.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200){
+                    listarAlabanzas(new String(responseBody));
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
 }
