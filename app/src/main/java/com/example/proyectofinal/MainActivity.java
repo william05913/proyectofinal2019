1f771cfc9b333de7fda5ba5f366b9c675498a750
william50913 <william29amaya@gmail.com>
2019-11-18 20:16:38 -0600

    le sigo agregando ala clase MainActivity ahora le agrege onOptionsItemSelected

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,6 +55,23 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
 
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.item_Alabanzas) {
+            Intent intent = new Intent(MainActivity.this, registro_alabanzas.class);
+            MainActivity.this.startActivity(intent);
+        }else if (id == R.id.item_Coros) {
+            Intent intent = new Intent(MainActivity.this, registro_coros.class);
+            MainActivity.this.startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
