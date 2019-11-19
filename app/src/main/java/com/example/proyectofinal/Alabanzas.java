c12657c6e9b1f47bde9813506dc8336b84e4e408
william50913 <william29amaya@gmail.com>
2019-11-18 20:52:19 -0600

    adicione un contructor vasio

--- app/src/main/java/com/example/proyectofinal/Alabanzas.java
+++ app/src/main/java/com/example/proyectofinal/Alabanzas.java
@@ -1,5 +1,11 @@
 package com.example.proyectofinal;
 
 public class Alabanzas {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
 
+    public Alabanzas() {
+    }
 }
