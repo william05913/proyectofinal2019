c28608c5c8d40dd36f67ef9580f3e7a3206c5528
Alexander12211997 <bonillaanderson304.07@gmail.com>
2019-11-19 08:26:09 -0600

    agregue el metodo listar_coros

--- app/src/main/java/com/example/proyectofinal/registro_coros.java
+++ app/src/main/java/com/example/proyectofinal/registro_coros.java
@@ -12,6 +12,10 @@ import android.widget.ListView;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
 import cz.msebera.android.httpclient.Header;
 
 public class registro_coros extends AppCompatActivity {
@@ -73,5 +77,19 @@ public class registro_coros extends AppCompatActivity {
             }
         });
     }
+    private  void listarCoros(String respuesta){
+        final ArrayList<Coros> lista = new ArrayList<Coros>();
+        try{
+            JSONArray jsonArreglo = new JSONArray(respuesta);
+            for (int i=0; i<jsonArreglo.length(); i++){
+                Coros a = new Coros();
+                a.setId(jsonArreglo.getJSONObject(i).getInt("id_c"));
+                a.setTitulo(jsonArreglo.getJSONObject(i).getString("titulo"));
+                a.setAutor(jsonArreglo.getJSONObject(i).getString("autor"));
+                a.setLetra(jsonArreglo.getJSONObject(i).getString("letra"));
+
+                lista.add(a);
+
+            }
     }
 
