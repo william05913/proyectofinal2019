bda625f1e3baf8d15988fa3cfa951e656cd0b793
Alexander12211997 <bonillaanderson304.07@gmail.com>
2019-11-19 07:29:14 -0600

    agregue el metodo onkeydowm ala clase registro_alabanzas

--- app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
+++ app/src/main/java/com/example/proyectofinal/registro_alabanzas.java
@@ -3,7 +3,9 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -22,6 +24,28 @@ public class registro_alabanzas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_alabanzas);
 
+
+        @Override
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+            if (keyCode == android.view.KeyEvent.KEYCODE_BACK) {
+                new android.app.AlertDialog.Builder(this)
+                        .setIcon(R.drawable.ic_close)
+                        .setTitle("Advertencia")
+                        .setIcon(R.drawable.ic_close)
+                        .setMessage("¿Realmente desea salir?")
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .show();
+                return true;
+            }
+            //para las demas cosas, se reenvia el evento al listener habitual
+            return super.onKeyDown(keyCode, event);
+        }
     }
 
 }
